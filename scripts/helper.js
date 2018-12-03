6041619190b7aa4913fd793b1a7b83a80eb1db81
wajiha <Wajiha.ahmed04@gmail.com>
2018-12-03 00:53:29 -0800

    assignment-11 completed

---
+++ scripts/helper.js
@@ -0,0 +1,12 @@
+
+class Helper{
+  playPauseAndUpdate(song) {
+    player.playPause(song);
+    const getDuration= player.getDuration();
+    $('.total-time').text(player.currentlyPlaying.duration);
+    $('#time-control').text(player.totalTime);
+  }
+}
+
+
+const helper = new Helper();
--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,6 +1,6 @@
 {
   $('button#play-pause').on('click', function() {
-    player.playPause();
+  helper.playPauseAndUpdate();
     $(this).attr('playState', player.playState);
   });
 
@@ -11,7 +11,7 @@
     if (nextSongIndex >= album.songs.length) { return; }
 
     const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
+    helper.playPauseAndUpdate(nextSong);
   });
 
   $('button#previous').on('click', function() {
@@ -22,12 +22,17 @@
     if (previousSongIndex >= album.songs.length) { return; }
 
     const previousSong = album.songs[previousSongIndex];
-    player.playPause(previousSong);
+    helper.playPauseAndUpdate(previousSong);
   });
 
     $('#time-control input').on('input', function (event) {
       player.skipTo(event.target.value);
     });
+
+    $('#volume-control input').on('input', function (event) {
+      player.setVolume(event.target.value);
+    });
+
 
   setInterval( () => {
     if (player.playState !== 'playing') { return; }
